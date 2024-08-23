6137fc254ea463f5690ea73611cb4b9995d5f2fb
corn <dalcon10280@gmail.com>
2024-08-24 01:31:49 +0900

    chore: add app launch script

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # market-broker-api
 
 > 증권사, 거래소 연동 API
+
+## Getting Started
+
+```bash
+poetry install
+poetry run start
+```
--- app/main.py
+++ app/main.py
@@ -1,3 +1,13 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+def start():
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
