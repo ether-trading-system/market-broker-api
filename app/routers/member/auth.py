6220b88eab03578aeae3c0c0ff4dc60c7c21e905
김대휘 <kimdaewhi@naver.com>
2024-11-11 17:16:21 +0900

    uvicorn 버전 변경

--- app/routers/member/auth.py
+++ app/routers/member/auth.py
@@ -1,7 +1,5 @@
 from fastapi import APIRouter
-from app.exceptions.kis_exception import kis_error_handler
-from core.config_manager import config_manager
-from core.settings import get_kis_api_info
+from pydantic import BaseModel
 import httpx
 
 router = APIRouter()
@@ -12,21 +10,26 @@ REQ_GET_ACCESS_TOKEN = "/oauth2/tokenP"     # 접속토큰 발급
 REQ_REMOVE_TOKEN = "/oauth2/revokeP"        # 접속토큰 폐기
 
 
+
+class TokenRequest(BaseModel):
+    url: str
+    api_key: str
+    app_secret: str
+
+
+
+
 @router.post("/get-token", tags=["auth"])
-async def get_token(url_kind: str):
-    config_manager.set("url_kind", url_kind)  # "simulate" 또는 "real"
-    
-    base_url = config_manager.get_base_url()
-    api_key = config_manager.get("KIS_APP_KEY")
-    app_secret = config_manager.get("KIS_APP_SECRET")
-    
-    print(get_kis_api_info())
+async def get_token(request: TokenRequest):
+    print(f"url : {request.url}")
+    print(f"api_key : {request.api_key}")
+    print(f"app_secret : {request.app_secret}")
     
     async with httpx.AsyncClient() as client:
-        response = await client.post(f"{base_url}{REQ_GET_ACCESS_TOKEN}", json={
+        response = await client.post(f"{request.url}{REQ_GET_ACCESS_TOKEN}", json={
             "grant_type": "client_credentials",  # 고정 값
-            "appkey": api_key,
-            "appsecret": app_secret,
+            "appkey": request.api_key,
+            "appsecret": request.app_secret,
         })
         
         return response.json()
