558af7bb9bf2bf219d45b1eb185a968f77bab601
kimdaewhi <kimdaewhi@naver.com>
2024-11-19 08:59:25 +0900

    사용하지 않는 commit 제거

--- app/routers/member/auth.py
+++ app/routers/member/auth.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
 from datetime import datetime
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
 from app.services.auth_service import AuthService
 
 import logging
